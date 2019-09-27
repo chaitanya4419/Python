f32b4db1d2e07b5ce68ecf2fb35365e0d0847a2e
chaitanya <chaicho1@in.ibm.com>
2019-09-27 09:46:10 +0530

    addition code in python

---
+++ python/addition.py
@@ -0,0 +1,23 @@
+print("Hello World")
+#create a variable x 
+x = 0
+#create a variable y
+y = 0
+#create a variable sum to store result
+sum = 0
+
+#Enter the value of x
+x = input("Enter X : ")
+#Enter the value of y
+y = input("Enter Y : ")
+
+#converting x to integer (type casting)
+x = int(x)
+#converting y to integer (type casting)
+y = int(y)
+
+#storing the result 
+sum = x + y
+
+#print result
+print('Sum = ',sum)
